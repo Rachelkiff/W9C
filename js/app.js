50d23c2833db829559b44afe097223bd1938e247
rachel <rachelkiffiak1@gmail.com>
2020-08-24 22:00:45 -0600

    Created an array of tweets

---
+++ js/app.js
@@ -0,0 +1,14 @@
+var tweets = ["Your current situation is not your final destination.", "Be brave enough to be bad at something new.", "If they say it’s impossible; it’s impossible for them, not you.", "You deserve a wild love from a gentle soul.", "Let it go. Something beautiful wants to grow in its place.", "Keep watering yourself until you feel like you again", "Pay attention to the things you are naturally drawn to. They are good for your soul.", "Don’t waste your energy on revenge. Your inner peace is more important.", "May the next few months be a period of beautiful transformation for you.", "Wherever you're at in your life, you're not alone."];
+var tweetlength = tweets.length;
+
+
+for(var counter = 0; counter < tweetlength; counter = counter + 1) {
+    console.log(tweets[counter]);
+}
+
+
+
+
+
+
+
